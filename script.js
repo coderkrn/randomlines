92b826f790120b79b8496e2aaeababcbc31ee4f0
Karan pal <108478303+coderkrn@users.noreply.github.com>
2022-09-10 22:01:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+let joke1 = ["Now I Realise That Losing You Was Actually A Gain...!",
+
+"You Are The Only One Who can change Your Future Son's Life...!",
+
+"You Are Crying for Girls Bruh someone Out There Is Challenging The Whole World...!",
+
+"Not Everyone Will See Your Vision don't Wait For Their Approval...!",
+
+"With Fake Nail, FAke Eyebrows, Fake Smile some Women They Will Get A Real Man...!"];
+let random = Math.floor(Math.random()*5);
+let line1 = document.getElementById('line1');
+if(random === 0){
+line1.innerText = joke1[0];
+}
+else if(random === 1){
+line1.innerText = joke1[1];
+}
+else if(random === 2){
+line1.innerText = joke1[2];
+}
+else if(random === 3){
+line1.innerText = joke1[3];
+}
+else if(random === 4){
+line1.innerText = joke1[4];
+}
+
+
+
+
+let joke2 = ["Don't Copy Anyone Do It Your Way, Be A Brand...!",
+
+"Everyone Has A Dream But Not Everyone Grind For It...!",
+
+"Sometimes You Are better Off Alone Nobody Can Hurt You :)",
+
+"The More You Know, The Less You Need To say...!",
+
+"You Have Only Two Choices Accept The Situation Or Change The Situation...!"];
+let random2 = Math.floor(Math.random()*5);
+let line2 = document.getElementById('line2');
+if(random2 === 0){
+line2.innerHTML = joke2[0];
+}
+else if(random2 === 1){
+line2.innerHTML = joke2[1];
+}
+else if(random2 === 2){
+line2.innerHTML = joke2[2];
+}
+else if(random2 === 3){
+line2.innerHTML = joke2[3];
+}
+else if(random2 === 4){
+line2.innerHTML = joke2[4];
+}
+
+
+let joke3 = ["I'm Going Through Many Types Of Pain Like Trust Issues, Family Issues, Fake friends, Sometimes When I Say I'm Okay, I Want Someone To Look Into My Eyes And Say I Know You're Not :)",
+
+"And Sometimes That Sadness gets So Deep In Your Heart... That's You Can't Even Cry :)",
+
+"I'm Strong Person. But Do You Know That Makes ME Cry? It's When I gave My Best But Still I Was Not Being Enough",
+
+"People Used TO Play With Toys And Now They Found A New Toy Known As FEELING :)",
+
+"Don't Force Anyone To Stay With Me, If You Find Yourself Happy Without Me Then Please Be Happy :)"];
+let random3 = Math.floor(Math.random()*5);
+let line3 = document.getElementById('line3');
+if(random3 === 0){
+line3.innerHTML = joke3[0];
+}
+else if(random3 === 1){
+line2.innerHTML = joke3[1];
+}
+else if(random3 === 2){
+line3.innerHTML = joke3[2];
+}
+else if(random3 === 3){
+lin3.innerHTML = joke3[3];
+}
+else if(random3 === 4){
+line3.innerHTML = joke3[4];
+}
