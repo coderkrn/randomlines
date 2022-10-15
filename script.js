966b218e89bbccae8f47995bd68deaf37092160b
Karan pal <108478303+coderkrn@users.noreply.github.com>
2022-10-15 10:40:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let joke1 = ["Now I Realise That Losing You Was Actually A Gain...!",
+
+"You Are The Only One Who can change Your Future Son's Life...!",
+
+"You Are Crying for Girls Bruh someone Out There Is Challenging The Whole World...!",
+
+"Not Everyone Will See Your Vision don't Wait For Their Approval...!",
+
+"With Fake Nail, FAke Eyebrows, Fake Smile some Women They Will Get A Real Man...!"];
+
+let line1 = document.getElementById('line1');
+
+let index = Math.floor(Math.random() * joke1.length );
+line1.innerHTML = joke1[index];
+
+
+
+
+let joke2 = ["Don't Copy Anyone Do It Your Way, Be A Brand...!",
+
+"Everyone Has A Dream But Not Everyone Grind For It...!",
+
+"Sometimes You Are better Off Alone Nobody Can Hurt You :)",
+
+"The More You Know, The Less You Need To say...!",
+
+"You Have Only Two Choices Accept The Situation Or Change The Situation...!"];
+
+let line2 = document.getElementById('line2');
+
+let index2 = Math.floor(Math.random() * joke1.length );
+line2.innerHTML = joke1[index2];
+
+
+let joke3 = ["I'm Going Through Many Types Of Pain Like Trust Issues, Family Issues, Fake friends, Sometimes When I Say I'm Okay, I Want Someone To Look Into My Eyes And Say I Know You're Not :)",
+
+"And Sometimes That Sadness gets So Deep In Your Heart... That's You Can't Even Cry :)",
+
+"I'm Strong Person. But Do You Know That Makes ME Cry? It's When I gave My Best But Still I Was Not Being Enough",
+
+"People Used TO Play With Toys And Now They Found A New Toy Known As FEELING :)",
+
+"Don't Force Anyone To Stay With Me, If You Find Yourself Happy Without Me Then Please Be Happy :)"];
+
+let line3 = document.getElementById('line3');
+
+let index3 = Math.floor(Math.random() * joke1.length );
+line3.innerHTML = joke1[index2];
